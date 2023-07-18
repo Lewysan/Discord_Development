3e066b04162b0f7cb8b70ced61590a242a0a6f3c
iNety_x <70720366+Lewysan@users.noreply.github.com>
2023-07-18 08:29:38 -0700

    Update index.js

--- 🤖) Anonwall-BOT (Deprecated)/index.js
+++ 🤖) Anonwall-BOT (Deprecated)/index.js
@@ -1,29 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 // const Discord = require('discord.js');
-const config = require('./resources/config.json')
 
-client.on('ready', () => {
-    console.log("✔ El bot está listo para ser usado.");
-    client.user.setPresence({
-        game: {
-            name: "StellarSquad Community",
-            type: "WATCHING"
-        }
-    })
-});
 
-client.on("message", (message) => {
-
-    if (message.content === "prueba"){
-        var date = new Date();
-        var horas = date.getHours();
-        var minutos = date.getMinutes();
-        return message.channel.send("Tiempo actual: "+horas+":"+minutos);
-    }
-    if (message.content === "help"){
-        return message.channel.send("Si necesitas ayuda contacta con el dueño: Tyranzx#0296");
-    }
-});
-
-client.login(config.TOKEN); 
+// Do it alone..
